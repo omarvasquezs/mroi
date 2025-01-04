0fc2545275f47169d9493b5b844f8116c3188f5b
Omar Vásquez <omarvs91@gmail.com>
2025-01-04 16:07:34 -0500

    Integración vite + ddev.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+const port = 5173;
+const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`;
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -25,4 +28,10 @@ export default defineConfig({
             vue: 'vue/dist/vue.esm-bundler.js',
         },
     },
+    server: {
+        host: '0.0.0.0',
+        port: port,
+        strictPort: true,
+        origin: origin
+    }
 });
