2a7d7d736735c06d715721e7a91562cfcaa4c31f
Omar Vásquez <omarvs91@gmail.com>
2025-01-04 16:50:46 -0500

    Actualizando rutas.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('template');
+});
+
+Route::get('/pacientes/agregar', function () {
+    return view('pacientes');
+})->name('pacientes.agregar');
+
+Route::get('/', function () {
+    return view('main');
 });
