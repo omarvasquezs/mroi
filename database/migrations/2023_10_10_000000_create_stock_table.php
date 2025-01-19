9b78f69e17bf34cb2140ac7c42b2e05f49c9ffd9
Omar Vásquez <omarvs91@gmail.com>
2025-01-19 09:28:27 -0500

    Adding stock migration.

---
+++ database/migrations/2023_10_10_000000_create_stock_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->id();
+            $table->string('producto');
+            $table->string('imagen');
+            $table->decimal('precio', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock');
+    }
+}
