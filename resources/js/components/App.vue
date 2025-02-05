9b3fc55364bfb06a73ecf7eb64413f4d93e2c7d8
Omar Vásquez <omarvs91@gmail.com>
2025-02-04 19:55:17 -0500

    Implementing conditional rendering for LoginForm and Layout in App.vue; updating welcome.blade.php to simplify app mount logic.

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,5 +1,24 @@
 <template>
-    <Layout />
+    <div>
+        <template v-if="$route.path === '/login'">
+            <LoginForm />
+        </template>
+        <template v-else>
+            <Layout />
+        </template>
+    </div>
 </template>
+
+<script>
+import LoginForm from './LoginForm.vue';
+import Layout from '@/layouts/index.vue';
+
+export default {
+    components: {
+        LoginForm,
+        Layout
+    }
+}
+</script>
 
 <style scoped></style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,15 +9,11 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/js/login.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body class="custom-body d-flex flex-column min-vh-100">
-    @if (request()->is('login'))
-        <div id="login" class="flex-grow-1"></div>
-    @else
-        <div id="app" class="flex-grow-1"></div>
-    @endif
+    <div id="app" class="flex-grow-1"></div>
     @vite('resources/js/main.js')
 </body>
 
