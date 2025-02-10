92b797cd21e09baf2ac63f98b023455d413f07af
Omar Vásquez <omarvs91@gmail.com>
2025-02-09 20:50:08 -0500

    Adding migration for productos_comprobante table with fields for comprobante_id, producto_id, cantidad, and precio.

---
+++ database/migrations/2025_02_09_170511_create_productos_comprobante_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos_comprobante', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('comprobante_id');
+            $table->unsignedBigInteger('producto_id');
+            $table->integer('cantidad');
+            $table->decimal('precio', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos_comprobante');
+    }
+};
