4677a8e4f780eae0585ad85456500efd28dd1d58
Omar Vásquez <omarvs91@gmail.com>
2025-02-19 20:31:27 -0500

    Updating PDF modal to use fullscreen layout for improved viewing experience.

--- resources/js/components/Facturacion.vue
+++ resources/js/components/Facturacion.vue
@@ -61,14 +61,14 @@
 
         <!-- PDF Modal -->
         <div class="modal fade" id="pdfModal" tabindex="-1" aria-labelledby="pdfModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg">
+            <div class="modal-dialog modal-fullscreen">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="pdfModalLabel">Comprobante {{ modalComprobanteNumero }}</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <iframe :src="pdfUrl" width="100%" height="500px"></iframe>
+                        <iframe :src="pdfUrl" width="100%" height="100%"></iframe>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
